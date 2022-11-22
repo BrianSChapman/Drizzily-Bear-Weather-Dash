6d456e183ea00b1ed972f0a2f3cb17e7e2fb1ef5
Brian Chapman <chapman.brian86@gmail.com>
2022-11-21 22:56:11 -0500

    Reworking JS to do some dynamic building of cards

--- script.js
+++ script.js
@@ -1,52 +1,58 @@
 var APIKey = "59ae0903b9c09ab9e0fdda3cbdca2806";
-var city;
+var city = [];
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-var userInput = document.getElementsByClassName("user-input")
-var searchButton = document.getElementsByClassName("search-button")
-var historyBox = document.getElementById("search-history")
-var mainPane = document.getElementById("search-result")
-var fiveDay = document.getElementById("five-day-forecast")
- 
+var userInput = document.querySelector(".user-input");
+var searchButton = document.querySelector(".search-button");
+var historyBox = document.querySelector("#search-history");
+var mainPane = document.querySelector("#today-weather");
+var fiveDay = document.querySelector("#five-day-container");
+
 
 var searchSubmission = function (event) {
-event.preventdefault();
+    // event.preventdefault();
 
 var  userSearch = userInput.value.trim();
     
     if(userSearch) {
-    getTheWeather(city);
+    getTheWeather(userSearch);
     
         mainPane.textContent = "";
         fiveDay.textContent = "";
-        
-
     }
-        
 }
 
-var getTheWeather = function() {
+ 
 
-    fetch(queryURL)
-    .then(function (response) {
-       if (response.ok) {
+
+var getTheWeather = function () {
+    var weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" +lat+ "&lon=" +lon+ "&appid=" + APIKey
+
+     fetch(weatherURL)
+        .then(function (response) {
+        if (response.ok) {
         console.log(response);
-        response.json()
-        .then(function (data) {
-        var fiveDayCards = document.createElement("div");
-        fiveDayCards.setAttribute("id", "five-day-forecast");
-            
-
-
-
-        
+        response.json().then(function (data) {
+        console.log(data);
+        displayForecast(temp,wind,humidity);
         })
+    } 
+})
 
-       } 
-       
-        
-    })
 
 }
+var displayForecast = function() {
+        var todaysWeather = document.createElement("div");
+        todaysWeather.setAttribute("id", "search-result");
+        var fiveDayCards = document.createElement("div");
+        fiveDayCards.setAttribute("id", "five-day-container");
+        mainPane.appendChild(todaysWeather);
+        fiveDay.appendChild(fiveDayCards);
+};
+
+        searchButton.addEventListener("click", searchSubmission);
+        userInput.addEventListener("submit", searchSubmission);
+        
+ 
 
 
 
