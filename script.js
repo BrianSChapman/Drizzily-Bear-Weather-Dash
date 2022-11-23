7f6eb8962b3e50899c5341545d3d493611374f9c
Brian Chapman <chapman.brian86@gmail.com>
2022-11-22 23:07:41 -0500

    Attempted to rework functions to append cards to HTML. May submit and come back

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 var APIKey = "59ae0903b9c09ab9e0fdda3cbdca2806";
-var city = [];
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userSearch + "&appid=" + APIKey;
 var userInput = document.querySelector(".user-input");
 var searchButton = document.querySelector(".search-button");
 var historyBox = document.querySelector("#search-history");
 var mainPane = document.querySelector("#today-weather");
 var fiveDay = document.querySelector("#five-day-container");
-var  userSearch = userInput.value.trim();
+var  userSearch = document.querySelector("#floatingInput").value.trim();
 
 var searchSubmission = function (event) {
-    event.preventdefault();
+    event.preventDefault();
 
     if(userSearch) {
     getTheWeather(userSearch);
@@ -19,8 +18,14 @@ var searchSubmission = function (event) {
     }
 }
 
- 
+fetch(queryURL)
+.then(function (response) {
+    if(response.ok) {
+    console.log(response); 
+    response.json().then(function (data){
 
+var lat = data.city.coord.lat;
+var lon = data.city.coord.lon;
 
 var getTheWeather = function (userSearch) {
     var weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" +lat+ "&lon=" +lon+ "&appid=" + APIKey
@@ -31,13 +36,14 @@ var getTheWeather = function (userSearch) {
         console.log(response);
         response.json().then(function (data) {
         console.log(data);
-        displayForecast(temp,wind,humidity);
+        displayForecast(data);})
+            } 
         })
-    } 
-})
+    }
+ }
+)}
+});
 
-
-}
 var displayForecast = function() {
         var todaysWeather = document.createElement("div");
         todaysWeather.setAttribute("id", "search-result");
@@ -46,6 +52,14 @@ var displayForecast = function() {
         mainPane.appendChild(todaysWeather);
         fiveDay.appendChild(fiveDayCards);
 };
+
+displaySearchHistory(data) {
+    var searchHistory = document.createElement("button");
+    searchHistory.setAttribute("class", "btn btn-secondary");
+    searchHistory.textContent = data.city.name;
+    historyBox.append(searchHistory);
+
+}
 
         searchButton.addEventListener("click", searchSubmission);
         userInput.addEventListener("submit", searchSubmission);
