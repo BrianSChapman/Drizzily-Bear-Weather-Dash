6aca06ebb5a68f513eb7dd2b446656d9f51a5872
Brian Chapman <chapman.brian86@gmail.com>
2022-12-09 17:29:28 -0500

    Refactored JS to try and fix API call functionality

--- script.js
+++ script.js
@@ -1,76 +1,71 @@
 var APIKey = "59ae0903b9c09ab9e0fdda3cbdca2806";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userSearch + "&appid=" + APIKey;
-var userInput = document.querySelector(".user-input");
 var searchButton = document.querySelector(".search-button");
 var historyBox = document.querySelector("#search-history");
 var mainPane = document.querySelector("#today-weather");
 var fiveDay = document.querySelector("#five-day-container");
-var  userSearch = document.querySelector("#floatingInput").value.trim();
 
 var searchSubmission = function (event) {
-    event.preventDefault();
-
-    if(userSearch) {
-    getTheWeather(userSearch);
-    
-        mainPane.textContent = "";
-        fiveDay.textContent = "";
-    }
-}
-
-fetch(queryURL)
-.then(function (response) {
-    if(response.ok) {
-    console.log(response); 
-    response.json().then(function (data){
-
-var lat = data.city.coord.lat;
-var lon = data.city.coord.lon;
-
-var getTheWeather = function (userSearch) {
-    var weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" +lat+ "&lon=" +lon+ "&appid=" + APIKey
-
-     fetch(weatherURL)
-        .then(function (response) {
-        if (response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-        console.log(data);
-        displayForecast(data);})
-            } 
-        })
-    }
- }
-)}
-});
-
-var displayForecast = function() {
-        var todaysWeather = document.createElement("div");
-        todaysWeather.setAttribute("id", "search-result");
-        var fiveDayCards = document.createElement("div");
-        fiveDayCards.setAttribute("id", "five-day-container");
-        mainPane.appendChild(todaysWeather);
-        fiveDay.appendChild(fiveDayCards);
+  event.preventDefault();
+  var userInput = document.querySelector(".user-input").value.trim();
+  console.log(userInput);
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    userInput +
+    "&units=imperial&appid=" +
+    APIKey;
+  if (!userInput) {
+    alert("Please enter a city!");
+    return;
+  } else {
+    fetch(queryURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        if (data.cod == 404) {
+          alert("City not found!");
+          return;
+        }
+        displayDaily(data);
+      });
+  }
 };
 
-displaySearchHistory(data); {
-    var searchHistory = document.createElement("button");
-    searchHistory.setAttribute("class", "btn btn-secondary");
-    searchHistory.textContent = data.city.name;
-    historyBox.append(searchHistory);
+// var getTheWeather = function (userSearch) {
+//     var weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" +lat+ "&lon=" +lon+ "&appid=" + APIKey
 
-}
+//      fetch(weatherURL)
+//         .then(function (response) {
+//         if (response.ok) {
+//         console.log(response);
+//         response.json().then(function (data) {
+//         console.log(data);
+//         displayForecast(data);})
+//             }
+//         })
+//     }
 
-        searchButton.addEventListener("click", searchSubmission);
-        userInput.addEventListener("submit", searchSubmission);
-        
- 
+var displayDaily = function (data) {
+//   console.log(data);
+  var cityNameEl = $("<h2>").addClass("card-title").text(data.name);
+  var tempEl = $("<p>").addClass("card-text").text(data.main.temp);
 
+  // var todaysWeather = document.createElement("div");
+  // todaysWeather.setAttribute("id", "search-result");
+  // var fiveDayCards = document.createElement("div");
+  // fiveDayCards.setAttribute("id", "five-day-container");
+  $('#today-weather').append(cityNameEl, tempEl);
 
+  // fiveDay.appendChild(fiveDayCards);
+};
 
+// function displaySearchHistory(data) {
+//     var searchHistory = document.createElement("button");
+//     searchHistory.setAttribute("class", "btn btn-secondary");
+//     searchHistory.textContent = data.city.name;
+//     historyBox.append(searchHistory);
 
+// }
 
-
-
-
-    
+searchButton.addEventListener("click", searchSubmission);
+// userInput.addEventListener("submit", searchSubmission);
